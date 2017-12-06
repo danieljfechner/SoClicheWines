eee5722997bd32077152aea232f74794ef97399b
winniedawg <danfechner@gmail.com>
2017-12-06 00:36:20 -0600

    Create a new repository for SoClicheWines

---
+++ SoClicheWines/SoClicheWines/Scripts/app/MainController.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module('myApp');
+//var mainController = function ($scope) {
+//    $scope.name = 'hello';
+//};
+app.controller('MainController', ['$scope', function ($scope) {
+    $scope.name = 'hello';    
+    $scope.mood = function (value) { return value == '0' ? 'Bad' : 'Good'; }
+}]);
+
+//app.controller('MainController', ['$scope', function ($scope) {
+//    $scope.mood = function (value) { return value == '0' ? 'Bad' : 'Good'; }
+//}]);
+//app.controller('MainController', ['$scope', mainController]);
---
+++ SoClicheWines/SoClicheWines/Scripts/app/app.js
@@ -0,0 +1,12 @@
+﻿var myApp = angular.module('myApp', ['ngRoute', 'ngResource']);
+
+myApp
+    .config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider
+                .when('/start', { templateUrl: './home/main', controller: 'MainController' })
+                .otherwise({ redirectTo: '/start' });
+        }])
+    .controller('RootController', ['$scope', '$route', '$routeParams', '$location',
+        function ($scope, $route, $routeParams, $location) {
+        }]);
