9a25c7bfd413f7e59ab1b6e1e6feed76392e9c89
winniedawg <danfechner@gmail.com>
2017-12-06 00:50:08 -0600

    Test commit

--- SoClicheWines/SoClicheWines/Controllers/HomeController.cs
+++ SoClicheWines/SoClicheWines/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace SoClicheWines.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View();//fake change
         }
 
         public ActionResult About()
