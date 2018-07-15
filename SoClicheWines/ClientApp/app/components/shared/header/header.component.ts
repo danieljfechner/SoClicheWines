57a1e06a4c31961c1a9c463261d2507d333d119c
winniedawg <danfechner@gmail.com>
2018-07-15 15:57:22 -0600

    Blah blah blah

---
+++ SoClicheWines/ClientApp/app/components/shared/header/header.component.ts
@@ -0,0 +1,14 @@
+﻿
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'shared-header',
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+    name: string;
+
+    constructor() {
+        this.name = 'Sam';
+    }
+}
